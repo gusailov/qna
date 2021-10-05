ed095f437a564c1c4f58dea7748cabf993b904b2
gusailov <gusailov@gmail.com>
2021-10-05 13:52:47 +0300

    procfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "qna_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: '167.172.191.23' }
+  config.action_mailer.default_url_options = { host: 'qna-gusailov.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-redis = { url: 'redis://localhost:6379/4' }
+redis = { url: 'redis://redistogo:4c04a08a24ee46b9e01f14195b3b5c7a@tarpon.redistogo.com:9003/4' }
 Sidekiq.configure_client do |config|
   config.redis = redis
 end
