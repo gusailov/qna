992209c05edc082b9d3503b66ed66c6fcef2e357
gusailov <gusailov@gmail.com>
2021-10-05 12:58:25 +0300

    gem redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -127,4 +127,19 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  #
+  cache_servers = %w(redis://cache-01:6379/0 redis://cache-02:6379/0)
+  config.cache_store = :redis_cache_store, { url: cache_servers,
+
+                                             connect_timeout: 30, # Defaults to 20 seconds
+                                             read_timeout: 0.2, # Defaults to 1 second
+                                             write_timeout: 0.2, # Defaults to 1 second
+                                             reconnect_attempts: 1, # Defaults to 0
+
+                                             error_handler: -> (method:, returning:, exception:) {
+                                               # Report errors to Sentry as warnings
+                                               Raven.capture_exception exception, level: 'warning',
+                                                                       tags: { method: method, returning: returning }
+                                             }
+  }
 end
