90801b0c5567361b9317fce247738f781f26060e
gusailov <gusailov@gmail.com>
2021-10-06 09:36:49 +0300

    minor fixes in comment.js and comments_channel.js

--- app/assets/javascripts/comments_channel.js
+++ app/assets/javascripts/comments_channel.js
@@ -1,27 +1,28 @@
 document.addEventListener("turbolinks:load", () => {
-  App.cable.subscriptions.create(
-    {
-      channel: "CommentsChannel",
-    },
-    {
-      connected() {},
+    App.cable.subscriptions.create(
+        {
+            channel: "CommentsChannel",
+        },
+        {
+            connected() {
+            },
 
-      disconnected() {
-        this.perform("unsubscribed");
-      },
+            disconnected() {
+                this.perform("unsubscribed");
+            },
 
-      received(data) {
-        let list = document.getElementById(
-          `${data.commentable_type.toLowerCase()}-${
-            data.commentable_id
-          }-comments`
-        );
+            received(data) {
+                let list = document.getElementById(
+                    `${data.commentable_type.toLowerCase()}-${
+                        data.commentable_id
+                    }-comments`
+                );
 
-        list.insertAdjacentHTML(
-          "beforeend",
-          `<div class="comment" id="comment-${data.id}">${data.body}</div>`
-        );
-      },
-    }
-  );
+                list.insertAdjacentHTML(
+                    "beforeend",
+                    `<div class="comment card m-2 text-muted" id="comment-${data.id}">${data.body}</div>`
+                );
+            },
+        }
+    );
 });
