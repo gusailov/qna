2e9624f6bcee8bef86c0a57f27c076f4b6e62633
gusailov <gusailov@gmail.com>
2021-10-15 15:48:51 +0300

    move redis url to credential

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-redis = { url: 'redis://:p4878f8b0bdfe284496ca43d781f9a27c9fc0818dfca324c35ebebbc2b76bdaf8@ec2-3-94-172-123.compute-1.amazonaws.com:22889' }
+redis = { url: Rails.application.credentials[:redis][:url] }
 Sidekiq.configure_client do |config|
   config.redis = redis
 end
